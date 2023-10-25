d6c2d9b2c7e903f7e02e5a30f6efa3cb4ea0f563
Volodymyr Maiorov <maiorov.volodymyr.v@gmail.com>
2023-10-25 17:25:02 +0200

    fixit import img, name of file styles

--- src/features/NotFound/NotFound.tsx
+++ src/features/NotFound/NotFound.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import './_404.scss';
+import image404 from '../../img/other/404.png';
+import './NotFound.module.scss';
 
 const NotFound: React.FC = (): JSX.Element => {
 	return (
@@ -11,8 +12,8 @@ const NotFound: React.FC = (): JSX.Element => {
 						<div className="error__content text-center">
 							<img
 								className="error__content--img mb-50"
-								src="./src/img/other/404-thumb.png"
-								alt="error-img"
+								src={image404}
+								alt="404 error / page not found"
 							/>
 							<h2 className="error__content--title">Opps! We did not find this page</h2>
 							<p className="error__content--desc">But you can return to the main page</p>
