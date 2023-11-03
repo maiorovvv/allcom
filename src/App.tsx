025974d875e454ca41c12f7a592c3efe7f58aa30
Igor Klein <igorklein1988@gmail.com>
2023-11-03 20:57:30 +0100

    add wish list user products and add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useTranslation } from 'react-i18next';
-import NotFound from './components/NotFound/NotFound';
 import Footer from './components/Footer/Footer';
-
-const locales = {
-	en: { title: 'English' },
-	de: { title: 'German' },
-	ru: { title: 'Russian' },
-};
+import NotFound from './components/NotFound/NotFound';
+import Products from './features/user/wishProducts/ProductList';
+import SelectLocale from './components/SelectLocale/SelectLocale';
 
 function App(): JSX.Element {
-	const { i18n } = useTranslation();
-
 	return (
-		<div>
-			<ul>
-				{Object.keys(locales).map((locale) => (
-					<li key={locale}>
-						<button
-							style={{ fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal' }}
-							type="submit"
-							onClick={() => i18n.changeLanguage(locale)}
-						>
-							{locales[locale as keyof typeof locales].title}
-						</button>
-					</li>
-				))}
-			</ul>
-
+		<>
+			<SelectLocale />
+			<Products />
 			<NotFound />
 			<Footer />
-		</div>
+		</>
 	);
 }
 
---
+++ src/features/user/wishProducts/ProductList.tsx
@@ -0,0 +1,66 @@
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { RootState } from '../../../app/store';
+import { loadProducts } from './productsSlice';
+import Product from './Product/Product';
+import Spinner from '../../../components/Spinner/Spinner';
+
+const ProductList = (): JSX.Element => {
+	const { t } = useTranslation('products');
+
+	const loading = useAppSelector((state: RootState) => state.userProducts.loading);
+	const products = useAppSelector((state: RootState) => state.userProducts.products);
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		dispatch(loadProducts());
+	}, []);
+
+	if (loading)
+		return (
+			<div className="text-center">
+				<Spinner />
+			</div>
+		);
+
+	return (
+		<section className="cart__section section--padding">
+			<div className="container">
+				<div className="cart__section--inner">
+					<form action="#">
+						<h2 className="cart__title mb-40">Wishlist</h2>
+						<div className="cart__table">
+							<table className="cart__table--inner">
+								<thead className="cart__table--header">
+									<tr className="cart__table--header__items">
+										<th className="cart__table--header__list">{t('product')}</th>
+										<th className="cart__table--header__list">{t('price')}</th>
+										<th className="cart__table--header__list text-right">{t('add_to_cart')}</th>
+									</tr>
+								</thead>
+								<tbody className="cart__table--body">
+									{products.map((product) => (
+										<tr className="cart__table--body__items" key={product.id}>
+											<Product product={product} />
+										</tr>
+									))}
+								</tbody>
+							</table>
+							<div className="cart__continue__shopping d-flex justify-content-between">
+								<a className="cart__continue__shopping--link" href="index.html">
+									Continue shopping
+								</a>
+								<a className="cart__continue__shopping--clear" href="shop.html">
+									View All Products
+								</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default ProductList;
