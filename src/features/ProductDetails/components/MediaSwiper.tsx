05a82754daaa6fd5ac7c5d9ef5e464d6f0e3f9f0
Volodymyr Maiorov <maiorov.volodymyr.v@gmail.com>
2023-11-25 00:37:54 +0100

    fix typeof setThumbsSwiper

--- src/features/ProductDetails/components/MediaSwiper.tsx
+++ src/features/ProductDetails/components/MediaSwiper.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { CSSProperties, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
@@ -8,25 +8,28 @@ import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 
 import { useTranslation } from 'react-i18next';
-
 import ProductDetailsInterface from '../types/ProductDetails';
 
 interface ProductProps {
 	product: ProductDetailsInterface;
 }
+
 const MediaSwiper = (props: ProductProps): JSX.Element => {
+	const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
 	const { product } = props;
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
-	const { t, i18n } = useTranslation('ProductDetails');
+	const { i18n } = useTranslation('ProductDetails');
 
 	return (
 		<div className="product__details--media">
 			<div className="product__media--preview swiper">
 				<Swiper
-					style={{
-						'--swiper-navigation-color': '#fff',
-						'--swiper-pagination-color': '#fff',
-					}}
+					style={
+						{
+							'--swiper-navigation-color': '#fff',
+							'--swiper-pagination-color': '#fff',
+						} as CSSProperties
+					}
+					loop={true}
 					spaceBetween={10}
 					navigation={true}
 					thumbs={{ swiper: thumbsSwiper }}
@@ -43,7 +46,6 @@ const MediaSwiper = (props: ProductProps): JSX.Element => {
 			<div className="product__media--nav swiper">
 				<Swiper
 					onSwiper={setThumbsSwiper}
-					loop={true}
 					spaceBetween={10}
 					slidesPerView={4}
 					freeMode={true}
