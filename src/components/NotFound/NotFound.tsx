bff35044bcb667c9d171bdd9a4e7fa4cf361390b
Tymofii Nikolenko <nikolenkote@gmail.com>
2024-01-24 01:36:35 +0100

    fix style and other in 404 page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,33 +1,24 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-import image404 from '../../img/other/404.png';
+import { Button } from 'react-bootstrap';
 
 const NotFound: React.FC = (): JSX.Element => {
 	const { t } = useTranslation('404-page');
 
+	const contentTitle = <h2 className="error__content--title">{t('content_title')}</h2>;
+	const contentDescription = <p className="error__content--desc">{t('content_desc')}</p>;
+	const buttonHome = (
+		<Button className="error__content--btn primary__btn" href="/">
+			{t('link_back_to_home')}
+		</Button>
+	);
 	return (
-		<section className="error__section section--padding">
-			<div className="container">
-				<div className="row row-cols-1">
-					<div className="col">
-						<div className="error__content text-center">
-							<img
-								className="error__content--img mb-50"
-								src={image404}
-								alt="404 error / page not found"
-							/>
-							<h2 className="error__content--title">{t('content_title')}</h2>
-							<p className="error__content--desc">{t('content_desc')}</p>
-							<Link className="error__content--btn primary__btn" to="/">
-								{t('link_back_to_home')}
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<div className="error__content">
+			{contentTitle}
+			{contentDescription}
+			{buttonHome}
+		</div>
 	);
 };
 
